231a6974c457cdca2a7a0a3289a81d7c464f6a5a
Jerry Lott <lottjerry@gmail.com>
2025-04-24 00:02:52 -0500

    first commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-footer
+    app
+    height="40"
+    class="d-flex justify-center align-center"
+  >
+  <p>2025. Missing Item Finder.</p>
+
+  </v-footer>
+</template>
+
+<script setup>
+ 
+</script>
+
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height d-flex justify-center align-center" max-width="900">
+    <div>
+      <v-img class="mb-4" height="150" src="@/assets/logo.png" />
+
+      <div class="mb-8 text-center">
+        <h1 class="text-h2 font-weight-bold">Missing Item Finder</h1>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script setup></script>
